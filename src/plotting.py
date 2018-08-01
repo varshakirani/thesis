cdf50b8c627835679a11cfd44f95998827b0269c
varsha@mac <varshakirani@gmail.com>
2018-08-01 09:59:24 +0200

    subploats for different models and classes are plotted for single contrast

---
+++ src/plotting.py
@@ -0,0 +1,52 @@
+import os
+import json
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import array
+from statistics import mean
+import pandas as pd
+import seaborn as sns
+
+
+
+def parse_options():
+    parser = ArgumentParser()
+    parser.add_argument("-o", "--output", required=False,
+                        default="outputs", type=str,
+                        help="Path to output folder")
+
+    options = parser.parse_args()
+    return options
+
+def main():
+    options = parse_options()
+    scoresdf = pd.read_csv(options.output+'without_kfold.csv')
+    models = scoresdf['Model'].unique()
+    nClass = scoresdf['Classifier'].unique()
+    contrasts = scoresdf['Contrast_name'].unique()
+    for contrast in contrasts:
+        fig, axes = plt.subplots(nrows=4, ncols=3)
+        axs = axes.ravel()
+        j = 0
+        for nc in nClass:
+            for model_name in models:
+
+                #sns.boxplot(x='Model', y='Score',
+                 #           data=scoresdf[(scoresdf['Type'] == 'test') & (scoresdf['Model'] == model_name)], ax=axs[j])
+                ax = sns.boxplot(x='Model',y='Score', hue='Type',data=scoresdf[(scoresdf['Model'] == model_name) & (scoresdf['Classifier'] == nc)], ax=axs[j])
+
+                axs[j].set_xlabel('')
+                axs[j].set_ylabel('')
+
+                j= j+1
+
+            axs[j-3].set_ylabel(str(nc))
+
+        print(contrast)
+        plt.suptitle(contrast)
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
