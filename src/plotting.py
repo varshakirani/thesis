715c28d677dff12a365da1c8d0533b7f04ff30c7
varsha@mac <varshakirani@gmail.com>
2018-08-01 11:15:43 +0200

    plotting of boxplot grid for different ml models and classes and saving the maximized figures onto output folder

--- src/plotting.py
+++ src/plotting.py
@@ -26,7 +26,7 @@ def main():
     nClass = scoresdf['Classifier'].unique()
     contrasts = scoresdf['Contrast_name'].unique()
     for contrast in contrasts:
-        fig, axes = plt.subplots(nrows=4, ncols=3)
+        fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(20, 20))
         axs = axes.ravel()
         j = 0
         for nc in nClass:
@@ -43,9 +43,18 @@ def main():
 
             axs[j-3].set_ylabel(str(nc))
 
-        print(contrast)
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())
+        #print(*mng.window.maxsize())
+
+        #plt.figure(figsize=(1440, 797))
+
         plt.suptitle(contrast)
-        plt.show()
+        plt.savefig("out/outputs_new/%s_withoutkfold.png"%(contrast))
+        plt.cla()
+        plt.clf()
+        plt.close()
+        #plt.show()
 
 
 if __name__ == '__main__':
